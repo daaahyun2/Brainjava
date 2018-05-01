a99962d3ab014f8d95f8e7db3a1d3d4478d4b248
kosea125 <kosea125@kdh>
2018-05-01 12:20:37 +0900

    p186 ex)4-15

---
+++ 뇌를자극하는자바/src/com/kook/firstjava/Conditiona10pExample1.java
@@ -0,0 +1,10 @@
+package com.kook.firstjava;
+
+public class Conditiona10pExample1 {
+	public static void main(String args[]) {
+		int a=20, b=30, max;
+		max=a<b?a:b;
+		System.out.println(max);
+	}
+
+}
