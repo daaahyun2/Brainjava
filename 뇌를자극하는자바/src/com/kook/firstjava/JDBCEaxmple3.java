71411cbf0fa1f28890ef414bdadc03909180afa0
kosea125 <kosea125@kdh>
2018-05-04 12:30:28 +0900

    180504 JDBC연동 - ex3 아까껀 클래스명이랑 다름 ㅜㅜ이게 진짜야

--- 뇌를자극하는자바/src/com/kook/firstjava/JDBCEaxmple3.java
+++ 뇌를자극하는자바/src/com/kook/firstjava/JDBCEaxmple3.java
@@ -2,12 +2,12 @@ package com.kook.firstjava;
 
 import java.sql.*;
 
-class JDBCExample3{
+class JDBCEaxmple3{
 		    public static void main(String args[]) {
 	    	//1단계:DB연결을 위한 커넥션 인터페이스
 	        Connection conn = null;
 	        //Statment 인터페이스 			- SQL 실행
-	        Statement stmt = null; //statmet=stmt (줄인거)
+	        Statement stmt = null; //statmet = stmt (줄인거)
 	        //ResultSet 인터페이스 			- SQL결과를 저장
 	        ResultSet rs = null;
 	        //try~catch문에서 DB연결중에 예외가 발생하는지를 검사.
@@ -27,7 +27,7 @@ class JDBCExample3{
 	            //DML SQL쿼리 실행 후 결과를 저장
 	            rs = stmt.executeQuery("select goodsinfo_code,	goodsinfo_name,	price,	maker from goodsinfo");
 	            
-	            System.out.println("상품코드	상품명	가격		제조사");
+	            System.out.println("상품코드	상품명	가격	제조사");
 	            
 	        while(rs.next()) {
 	            
